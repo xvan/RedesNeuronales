537b63d3caaa7855e7f81f0ba3baf48ded376ee5
Juan Zuloaga <jzuloaga@csc.conicet.gov.ar>
2022-05-03 18:44:42 -0300

    Haciendo el informe

--- TP1/generate_report.sh
+++ TP1/generate_report.sh
@@ -1,1 +1,2 @@
-sudo apt-get install texlive-xetex texlive-fonts-recommended texlive-plain-generic
+#sudo apt-get install texlive-xetex texlive-fonts-recommended texlive-plain-generic
+jupyter nbconvert --to pdf --template hidecode  --output "TP1_Zuloaga" TP1.ipynb
