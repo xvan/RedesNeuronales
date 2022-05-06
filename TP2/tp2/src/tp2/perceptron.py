1397f43c5a5caa3ad47d7202d453c5dd56105ad2
Juan Antonio Zuloaga Mellino <jzuloaga@fi.uba.ar>
2022-05-06 10:44:58 -0300

    arrr

--- TP2/tp2/src/tp2/perceptron.py
+++ TP2/tp2/src/tp2/perceptron.py
@@ -1,17 +1,17 @@
 import numpy as np
-from typing import Callable
+from typing import Callable, Tuple, List
 
 
 class Perceptron:
     import numpy as np
 
-    def __init__(self, activator):
-        self.activator: Callable[[np.ndarray], np.ndarray] = activator
-        self.weights: np.ndarray = np.array([1])
+    def __init__(self):
+        self.activator: Callable[[np.ndarray], np.ndarray] = None
+        self.weights: np.ndarray = None
 
     def process(self, xi: np.ndarray):
-        xi_fix = self.append_bias(xi)
-        h = self.weights * xi_fix
+        xi_biased = self.append_bias(xi)
+        h = np.dot(self.weights, xi_biased)
         return self.activate(h)
 
     def activate(self, h: np.ndarray):
@@ -19,3 +19,25 @@ class Perceptron:
 
     def append_bias(self, xi: np.ndarray) -> np.ndarray:
         return np.append(-1, xi)
+
+    def train(self, data: List[Tuple[np.ndarray,np.ndarray]]):
+        first_xi = data[0][0]
+        self.weights = self.append_bias(np.ones(np.shape(first_xi))).transpose()
+
+
+
+class ThresholdUnit(Perceptron):
+
+    @staticmethod
+    def sgn(xi: np.ndarray) -> np.ndarray:
+        return (xi >= 0).astype(int) * 2 - 1
+
+    def __init__(self):
+        super().__init__()
+        self.activator = self.sgn
+
+    def train(self, data) -> np.ndarray:
+        first_xi = data[0][0]
+        self.weights = self.append_bias(np.ones(np.shape(first_xi))).transpose()
+        self.weights[0]=0
+
--- TP2/tp2/test/test_tp2.py
+++ TP2/tp2/test/test_tp2.py
@@ -2,30 +2,64 @@ import unittest
 import numpy as np
 import numpy.testing as npt
 
-from tp2.perceptron import Perceptron
+from tp2.perceptron import Perceptron, ThresholdUnit
 
 
 class TestPerceptron(unittest.TestCase):
-    def activator(self):
+    def activator(self, xi):
         return 1
 
-    perceptron = Perceptron(activator)
+    def test_Process_Throws_Without_Training(self):
+        per = Perceptron()
+        per.activator = lambda x: 1
+        with self.assertRaises(TypeError):
+            per.process(np.ndarray([1]))
 
-    def test_PerceptronProcess(self):
-        output = self.perceptron.process(np.array([1]))
+    def test_Process_With_Training(self):
+        per = Perceptron()
+        per.activator = lambda x: 1
+        per.train([([1],[1])])
+        per.process([1])
 
-    def test_Process_Throws_TypeError_on_bad_input(self):
-        bad_inputs = [None, "sorongo"]
+    def test_Process_Throws_on_bad_input(self):
+        bad_inputs = [None, "string"]
         for bad_input in bad_inputs:
             with self.assertRaises(TypeError):
                 print(bad_input)
-                self.perceptron.process(bad_input)
+                Perceptron().process(bad_input)
 
     def test_bias(self):
         test = [1, 2, 3]
         expected = [-1, 1, 2, 3]
-        npt.assert_array_equal(expected, self.perceptron.append_bias(test))
+        npt.assert_array_equal(expected, Perceptron().append_bias(test))
 
+    def test_and_gate(self):
+        data = [
+            ([1, 1], [1]),
+            ([1, 0], [0]),
+            ([0, 1], [0]),
+            ([1, 1], [0])
+        ]
+        perceptron = Perceptron()
+        perceptron.train(data)
+
+class TestThresholdUnits(unittest.TestCase):
+    def test_activator(self):
+        npt.assert_array_equal(np.array([1, 1, -1]), ThresholdUnit().activate(np.array([4, 0, -5])))
+
+    def test_process(self):
+        data=[
+            ([1, 1, 1], [1]),
+            ([1, 0, -1], [1]),
+            ([1, -1, -1], [-1]),
+        ]
+        tu=ThresholdUnit()
+        tu.train(data)
+
+        for (x, y) in data:
+            npt.assert_array_equal(y, tu.process(x))
+
+        #npt.assert_array_equal(np.array([1, 1, -1]), ThresholdUnit().activate(np.array([4, 0, -5])))
 
 if __name__ == '__main__':
     unittest.main()
