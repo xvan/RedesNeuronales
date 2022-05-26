00c12d04daf2a7f43e3493b3b9c56807eaf77ed2
Juan Antonio Zuloaga Mellino <jzuloaga@fi.uba.ar>
2022-05-25 21:46:50 -0300

    Explode multilayer training

--- TP2/tp2/src/tp2/multilayer.py
+++ TP2/tp2/src/tp2/multilayer.py
@@ -20,18 +20,17 @@ class MultilayerNetwork:
         return self.state[-1]
 
     def train(self, data: TrainDataType):
-        trainer = MultilayerTrainer(data)
-        trainer.train(self)
+        MultilayerTrainer(self, data).train()
 
 
 class MultilayerTrainer:
-
-    def __init__(self, data: TrainDataType):
+    def __init__(self, network: MultilayerNetwork, data: TrainDataType):
+        self.network = network
         self.learning_rate: float = 0.01
         self.iterations_limit: float = 100000
         self.data: TrainDataType = data
 
-    def _init_exit_buffer(self):
+    def _init_costs_log(self):
         buffer_size = 10
         self.last_costs = RingBuffer(buffer_size)
         self.long_costs = RingBuffer(buffer_size)
@@ -40,58 +39,93 @@ class MultilayerTrainer:
             self.last_costs.append(np.inf)
             self.long_costs.append(np.inf)
 
-    def train(self, network: MultilayerNetwork):
+    @property
+    def last_cost(self):
+        return self.last_costs[-1]
 
-        best_weights = None
-        best_cost = np.inf
+    @property
+    def mean_cost(self):
+        return self.long_costs[-1]
 
-        failed_attempts = 0
-        while failed_attempts < 10:
+    @property
+    def old_mean_cost(self):
+        return self.long_costs[0]
 
-            for perceptron in network.perceptrons:
-                perceptron.weights = np.random.rand(*np.shape(perceptron.weights)) * 2 - 1
-
-            self._init_exit_buffer()
-
-            for _ in range(self.iterations_limit):
-                np.random.shuffle(self.data)
-                cost = 0
-                for xo, y in self.data:
-
-                    # Set States
-                    network.process(xo)
-
-                    cost += network.perceptrons[-1].sample_cost(y)
-                    # BackPropagate Deltas
-
-                    network.perceptrons[-1].first_delta(y)
-
-                    for idx in list(range(len(network.perceptrons))[-2::-1]):
-                        propagated_delta = network.perceptrons[idx + 1].back_propagate_delta()
-                        network.perceptrons[idx].hidden_delta(propagated_delta)
-
-                    # update weights:
-
-                    for perceptron in network.perceptrons:
-                        perceptron.update_weights(self.learning_rate)
-
-                #print(cost)
-                self.last_costs.append(cost)
-                mean_cost = np.mean(self.last_costs)
-                self.long_costs.append(mean_cost)
-                if 0.999 * self.long_costs[0] < mean_cost:
-                    break
-
-            if cost > best_cost:
-                failed_attempts += 1
-            else:
-                failed_attempts = 0
-                best_weights = [np.copy(p.weights) for p in network.perceptrons]
-                best_cost = cost
-
-            if cost <= 0.005:
+    def train(self):
+        self._init_exit_control()
+        while self.failed_attempts < 10:
+            self._train_attempt()
+            self._update_exit_control()
+            if self.last_cost <= 0.005:
                 return
+        self._restore_best_weights()
 
-        for w, p in zip(best_weights, network.perceptrons):
+    def _restore_best_weights(self):
+        for w, p in zip(self.best_weights, self.network.perceptrons):
             p.weights = w
+
+    def _save_best_weights(self):
+        self.best_weights = [np.copy(p.weights) for p in self.network.perceptrons]
+
+    def _init_attempt_states(self):
+        self._random_initialize_weights()
+        self._init_costs_log()
+
+    def _init_exit_control(self):
+        self.best_weights = None
+        self.best_cost = np.inf
+        self.failed_attempts = 0
+
+    def _train_attempt(self):
+        self._init_attempt_states()
+        for _ in range(self.iterations_limit):
+            np.random.shuffle(self.data)
+            cost = self._train_step()
+            self._update_costs_log(cost)
+            if self._improvement_not_significant():
+                break
+
+    def _update_costs_log(self, cost):
+        # print(cost)
+        self.last_costs.append(cost)
+        mean_cost = np.mean(self.last_costs)
+        self.long_costs.append(mean_cost)
+
+    def _train_step(self):
+        return np.sum([self._train_sample(xo, y) for xo, y in self.data])
+
+    def _train_sample(self, xo, y):
+        cost = self._set_network_states(xo, y)
+        self._backpropagate_deltas(y)
+        self._update_weights()
+        return cost
+
+    def _update_weights(self):
+        for perceptron in self.network.perceptrons:
+            perceptron.update_weights(self.learning_rate)
+
+    def _backpropagate_deltas(self, y):
+        self.network.perceptrons[-1].first_delta(y)
+        for idx in list(range(len(self.network.perceptrons))[-2::-1]):
+            propagated_delta = self.network.perceptrons[idx + 1].back_propagate_delta()
+            self.network.perceptrons[idx].hidden_delta(propagated_delta)
+
+    def _set_network_states(self, xo, y):
+        self.network.process(xo)
+        return self.network.perceptrons[-1].sample_cost(y)
+
+    def _random_initialize_weights(self):
+        for perceptron in self.network.perceptrons:
+            perceptron.weights = np.random.rand(*np.shape(perceptron.weights)) * 2 - 1
+
+    def _improvement_not_significant(self):
+        return 0.999 * self.old_mean_cost < self.mean_cost
+
+    def _update_exit_control(self):
+        if self.last_cost > self.best_cost:
+            self.failed_attempts += 1
+        else:
+            self.failed_attempts = 0
+            self.best_cost = self.last_cost
+            self._save_best_weights()
 
